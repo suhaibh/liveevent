6952d9433a3cfe75916cd92b0d37a48c0732f56f
Suhaib Habibullah <suhaib.habibullah@gmail.com>
2020-02-22 17:35:10 -0500

    Set up sass file structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "../src/main.scss";
+import "sass/main.scss";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
